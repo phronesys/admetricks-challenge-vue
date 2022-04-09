2ec86bda0e23c9b0f03f695060e33491b72d54e1
phronesys <daniel.dazarola@gmail.com>
2022-04-08 20:14:25 -0400

    feat(component): icon and png's

---
+++ src/components/AmIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <img v-if="iconUrl" :src="iconUrl" class="w-8 h-8" />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  name: {
+    type: String,
+    default: "device",
+  },
+});
+
+const iconUrl = new URL(`../assets/icons/icon-${props.name}.png`, import.meta.url).href;
+</script>
+
+<style lang="postcss"></style>
