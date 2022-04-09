9c680f79747e00dc5a5e1497ef229d3f82f519f2
phronesys <daniel.dazarola@gmail.com>
2022-04-09 18:07:11 -0400

    enhance(layout): card grid done

--- src/layouts/landing/AmCardGrid.vue
+++ src/layouts/landing/AmCardGrid.vue
@@ -1,15 +1,38 @@
 <template>
-  <div class="card-grid">
-    <am-card v-for="(n, index) in 5" :key="index"></am-card>
-  </div>
+  <section id="cards">
+    <div class="card-grid">
+      <am-card
+        v-for="(classes, index) in cardClasses"
+        :key="index"
+        :class="classes"
+      ></am-card>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
 import AmCard from "../../components/AmCard.vue";
+const cardClasses = [
+  "row-start-2 row-end-3 col-start-1 col-end-2 w-11/12 justify-self-end",
+  "row-start-2 row-end-4 col-start-2 col-end-3",
+  "row-start-1 row-end-4 col-start-3 col-end-4",
+  "row-start-3 row-end-5 col-start-1 col-end-2",
+  "row-start-4 row-end-6 col-start-2 col-end-4",
+];
 </script>
 
 <style lang="postcss">
-.card-grid {
-  @apply grid mx-auto h-[1000px] w-[1200px];
+section#cards {
+  @apply py-40;
+}
+section#cards .card-grid {
+  @apply h-[400px] w-full mx-auto px-8 flex flex-col sm:grid justify-center gap-4;
+  @apply sm:w-3/4 sm:h-[600px];
+  @apply lg:w-2/3 lg:h-[800px];
+  @apply xl:h-[1000px];
+  @apply 2xl:h-[1200px];
+
+  grid-template-columns: 35.5% 29% 35.5%;
+  grid-template-rows: 10% 18% 27% 37% 8%;
 }
 </style>
