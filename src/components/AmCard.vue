759854ae98d7f68c05520c77d4250be3bd5df753
phronesys <daniel.dazarola@gmail.com>
2022-04-09 02:16:38 -0400

    refactor(component): card

--- src/components/AmCard.vue
+++ src/components/AmCard.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="card" :class="{ alternative }">
+  <div class="card" :class="[{ alternative }]">
     <slot></slot>
   </div>
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
-  alternative: {
-    type: Boolean,
-    default: false,
-  },
-});
+const { alternative = false } = defineProps<{
+  alternative?: boolean;
+}>();
 </script>
 
 <style lang="postcss">
 .card {
-  @apply w-full h-full p-5 bg-[#EEF2FA] rounded-[40px];
+  @apply w-full h-full p-5 bg-admetricks-light rounded-[40px];
+
   &.alternative {
     @apply bg-admetricks-dark border border-white rounded-[32px];
   }
