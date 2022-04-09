e335090d3facc3e14880333ea9159325a12feb67
phronesys <daniel.dazarola@gmail.com>
2022-04-08 20:13:35 -0400

    feat(component): initial feature component

---
+++ src/components/AmFeature.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="feature">
+    <!-- icon -->
+    <am-icon></am-icon>
+    <h4>
+      <slot name="title"></slot>
+    </h4>
+    <p>
+      <slot name="body"></slot>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AmIcon from './AmIcon.vue';
+const props = defineProps({
+  iconName: {
+    type: String,
+    default: 'device'
+  }
+});
+</script>
+
+<style lang="postcss">
+.feature {
+  @apply flex flex-col gap-2;
+  & > h4 {
+    @apply text-lg text-white;
+  }
+}
+</style>
