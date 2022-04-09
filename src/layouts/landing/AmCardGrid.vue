6781f696b4a3242e6a8b180d73ceb2e0a0a03673
phronesys <daniel.dazarola@gmail.com>
2022-04-09 02:17:33 -0400

    feat(layout): initial card grid

---
+++ src/layouts/landing/AmCardGrid.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card-grid">
+    <am-card v-for="(n, index) in 5" :key="index"></am-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AmCard from "../../components/AmCard.vue";
+</script>
+
+<style lang="postcss">
+.card-grid {
+  @apply grid mx-auto h-[1000px] w-[1200px];
+}
+</style>
