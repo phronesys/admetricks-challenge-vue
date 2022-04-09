57e6fa8dc99fb1af5753c2f0d121910441a63e88
phronesys <daniel.dazarola@gmail.com>
2022-04-09 02:14:14 -0400

    feat(layout): features

---
+++ src/layouts/landing/AmFeatures.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="features">
+    <am-feature
+      v-for="feature in featuresContent"
+      :key="feature.title"
+      :iconName="feature.iconName"
+    >
+      <template #title> {{ feature.title }} </template>
+      <template #body> {{ feature.body }} </template>
+    </am-feature>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import AmFeature from "../../components/AmFeature.vue";
+
+const featuresContent = [
+  {
+    title: "Agencias",
+    body: `Crea mejores estrategias de marketing digital, planes de medios y
+        reportes de resultados para tus clientes. Muéstrale qué está haciendo su
+        competencia en publicidad online.`,
+    iconName: "idea",
+  },
+  {
+    title: "Medios",
+    body: `Mejora tus argumentos de ventas conociendo en detalle la inversión de 
+        tus clientes en otros medios y el alcance que les aporta.us clientes en 
+        otros medios y el alcance que les aporta.`,
+    iconName: "device",
+  },
+  {
+    title: "Marcas",
+    body: `Descubre las ofertas y campañas que tu competencia está promocionando. 
+        Recibe alertas cuando tu competencia lanza una nueva campaña de publicidad
+        online.`,
+    iconName: "megaphone",
+  },
+  {
+    title: "Verificación",
+    body: `Como un agente tercero, transparente y sin conflictos de interés, 
+        Admetricks permite a los actores de la industria del marketing digital 
+        verificar que sus campañas exhiban en los sitios y formatos pautados.`,
+    iconName: "research",
+  },
+];
+</script>
+
+<style lang="postcss">
+.features {
+  @apply flex flex-row gap-4 mx-auto max-w-[1080px] pb-20;
+}
+</style>
--- src/views/landing.vue
+++ src/views/landing.vue
@@ -1,40 +1,46 @@
 <template>
   <section id="landing">
-    <Header></Header>
-    <div class="devices">
-      <am-card alternative class="w-[530px] h-[405px]"></am-card>
-      <am-card alternative class="w-[260px] h-[486px]"></am-card>
-    </div>
+    <main>
+      <am-header></am-header>
+      <div class="devices">
+        <am-card alternative class="w-[530px] h-[405px]"></am-card>
+        <am-card alternative class="w-[260px] h-[486px]"></am-card>
+      </div>
+    </main>
 
-    <am-icon></am-icon>
+    <am-features></am-features>
+
+    <am-card-grid></am-card-grid>
   </section>
 </template>
 
 <script setup lang="ts">
 import AmCard from "../components/AmCard.vue";
-import AmIcon from "../components/AmIcon.vue";
-import Header from "../layouts/landing/Header.vue";
+import AmCardGrid from "../layouts/landing/AmCardGrid.vue";
+import AmFeatures from "../layouts/landing/AmFeatures.vue";
+import AmHeader from "../layouts/landing/AmHeader.vue";
 </script>
 
 <style lang="postcss">
 section#landing {
   @apply text-admetricks-gray bg-admetricks-dark;
-  @apply pl-[400px] pr-[270px] pt-[540px] pb-52 flex flex-row justify-center;
-
   /* using css instead of an image */
-  clip-path: polygon(50% 15%, 100% 0, 100% 100%, 0 100%, 0 0);
+  clip-path: polygon(50% 5%, 100% 0, 100% 100%, 0 100%, 0 0);
 }
 
-section#landing .devices {
-  @apply relative w-[680px];
-  & > .card {
-    @apply absolute;
+section#landing main {
+  @apply pl-[400px] pr-[270px] pt-[540px] pb-52 flex flex-row justify-center;
+  & .devices {
+    @apply relative w-[680px];
+    & > .card {
+      @apply absolute;
 
-    &:first-child {
-      @apply left-12;
-    }
-    &:last-child {
-      @apply right-0 -translate-y-1/2 top-1/2;
+      &:first-child {
+        @apply left-12;
+      }
+      &:last-child {
+        @apply right-0 -translate-y-1/2 top-1/2;
+      }
     }
   }
 }
