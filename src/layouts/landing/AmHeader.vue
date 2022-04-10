e3f149639d68250f02e2be9a5192d6364c40e283
phronesys <daniel.dazarola@gmail.com>
2022-04-09 21:42:07 -0400

    refactor(layout): responsiveness and structure

--- src/layouts/landing/AmHeader.vue
+++ src/layouts/landing/AmHeader.vue
@@ -1,63 +1,100 @@
 <template>
   <section id="header">
-    <div class="title">
-      <h5>Estamos para ayudarte</h5>
-      <h1>
-        ¿Tienes problemas <br />
-        para <span>entender el mercado</span> de la publicidad digital?
-      </h1>
+    <div class="content">
+      <div class="title">
+        <h5>Estamos para ayudarte</h5>
+        <h1>
+          ¿Tienes problemas <br v-if="screenWidth >= 640" />
+          para <span>entender el mercado</span> de la publicidad digital?
+        </h1>
+      </div>
+      <p class="body">
+        El Mercado de la publicidad online es incierto y complejo, Admetricks te
+        <span
+          >ayuda a visibilizar cómo desempeñan las campañas y contenidos</span
+        >
+        versus los de tu competencia en tiempo real con reportes y alertas
+        automáticas
+      </p>
+      <div class="buttons">
+        <am-button>Solicitar demo</am-button>
+        <am-button type="link">Ver funcionalidades</am-button>
+      </div>
     </div>
-    <p class="body">
-      El Mercado de la publicidad online es incierto y complejo, Admetricks te
-      <span>ayuda a visibilizar cómo desempeñan las campañas y contenidos</span>
-      versus los de tu competencia en tiempo real con reportes y alertas
-      automáticas
-    </p>
-    <div class="buttons">
-      <am-button>Solicitar demo</am-button>
-      <am-button type="link">Ver funcionalidades</am-button>
+    <div class="devices">
+      <am-card alternative class="w-[530px] h-[405px]"></am-card>
+      <am-card alternative class="w-[260px] h-[486px]"></am-card>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
 import AmButton from "../../components/AmButton.vue";
+import AmCard from "../../components/AmCard.vue";
+import { useWindowSize } from "@vueuse/core";
+
+const { width: screenWidth } = useWindowSize();
 </script>
 
 <style lang="postcss">
 section#header {
-  @apply flex flex-col gap-8 max-w-[532px];
+  @apply mx-auto p-4 flex;
+  @apply xl:w-[1180px];
+}
+section#header > .content {
+  @apply flex flex-col gap-8 w-full max-w-[400px] sm:max-w-[540px] pl-4;
 }
 
-section#header .title {
+section#header > .content > .title {
   @apply text-white font-semibold;
   & > h5 {
     @apply text-admetricks-primary;
   }
   & > h1 {
-    @apply text-4xl leading-normal;
+    @apply text-xl leading-normal;
+    @apply sm:text-4xl;
+    /* custom underline */
     span {
       @apply whitespace-nowrap relative z-10;
       &::before {
-        @apply content-[''] w-[400px] h-5 absolute bottom-0 -left-1.5 -z-10;
+        @apply content-[''] w-[200px] h-5 absolute bottom-0 -left-1.5 -z-10;
         @apply rounded-xl opacity-50;
         background: transparent
           linear-gradient(90deg, #00d4ff 0%, #aeda28 100%, #fe7117 100%) 0% 0%
           no-repeat padding-box;
+
+        @apply sm:w-[400px];
       }
     }
   }
 }
 
-section#header > .body {
-  @apply text-lg;
+section#header > .content > .body {
+  @apply sm:text-lg text-sm;
 
   span {
     @apply text-admetricks-primary font-semibold;
   }
 }
 
-section#header .buttons {
+section#header > .content > .buttons {
   @apply flex flex-row gap-2;
+}
+
+/* using cards */
+section#header > .devices {
+  @apply relative w-[540px] hidden;
+  @apply xl:block;
+
+  & > .card {
+    @apply absolute;
+
+    &:first-child {
+      @apply left-12;
+    }
+    &:last-child {
+      @apply right-0 -translate-y-1/2 top-1/2;
+    }
+  }
 }
 </style>
--- src/views/landing.vue
+++ src/views/landing.vue
@@ -1,21 +1,12 @@
 <template>
   <section id="landing">
-    <main>
-      <am-header></am-header>
-      <div class="devices">
-        <am-card alternative class="w-[530px] h-[405px]"></am-card>
-        <am-card alternative class="w-[260px] h-[486px]"></am-card>
-      </div>
-    </main>
-
+    <am-header></am-header>
     <am-features></am-features>
-
     <am-card-grid></am-card-grid>
   </section>
 </template>
 
 <script setup lang="ts">
-import AmCard from "../components/AmCard.vue";
 import AmCardGrid from "../layouts/landing/AmCardGrid.vue";
 import AmFeatures from "../layouts/landing/AmFeatures.vue";
 import AmHeader from "../layouts/landing/AmHeader.vue";
@@ -23,25 +14,9 @@ import AmHeader from "../layouts/landing/AmHeader.vue";
 
 <style lang="postcss">
 section#landing {
+  @apply flex flex-col mx-auto gap-40 pb-80 pt-96;
   @apply text-admetricks-gray bg-admetricks-dark;
   /* using css instead of an image */
   clip-path: polygon(50% 5%, 100% 0, 100% 100%, 0 100%, 0 0);
-}
-
-section#landing main {
-  @apply pl-[400px] pr-[270px] pt-[540px] pb-52 flex flex-row justify-center;
-  & .devices {
-    @apply relative w-[680px];
-    & > .card {
-      @apply absolute;
-
-      &:first-child {
-        @apply left-12;
-      }
-      &:last-child {
-        @apply right-0 -translate-y-1/2 top-1/2;
-      }
-    }
-  }
 }
 </style>
