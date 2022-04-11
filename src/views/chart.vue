7e0c45f5ed69798c05ab5bfd85f706298f141190
phronesys <daniel.dazarola@gmail.com>
2022-04-11 00:33:17 -0400

    feat(chart): initial chart with d3

--- src/views/chart.vue
+++ src/views/chart.vue
@@ -1,14 +1,42 @@
 <template>
-  <div class="bg-red-500 text-white w-max px-5 py-8">
-    aca va la chart
-  </div>
+  <main id="chart"></main>
 </template>
 
 <script setup lang="ts">
-/* import D3 */
+/* reference https://www.digitalocean.com/community/tutorials/getting-started-with-data-visualization-using-javascript-and-the-d3-library */
+import * as d3 from "d3";
+import { onMounted } from "vue";
 
+onMounted(() => {
+  const data = [
+    { name: "falabella", reach: 0.54, frequency: 8.13 },
+    { name: "paris", reach: 0.25, frequency: 3.06 },
+  ];
+
+  /* create svg */
+  const svg = d3
+    .select("#chart")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 500);
+
+  /* create rectangles */
+  svg
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("height", (dataPoint) => dataPoint.reach * 100)
+    .attr("width", 30)
+    .attr("x", (dataPoint, index) => index * 60 + 25)
+    .attr("y", (dataPoint) => 500 - (dataPoint.reach * 100));
+
+  /*  */
+});
 </script>
 
 <style lang="postcss">
-/* create layout */
+main#chart {
+  @apply grid place-items-center h-screen -mt-2;
+}
 </style>
