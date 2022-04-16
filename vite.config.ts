aa35992bb7b91b34aa1580906b30ecb4979c01f3
phronesys <daniel.dazarola@gmail.com>
2022-04-15 22:03:26 -0400

    feat: build web component as lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/web-components/index.ts'),
+      name: 'BarChart',
+      fileName: 'WCBarChart'
+    },
+    rollupOptions: {
+      external: ['vue', 'd3'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          d3: 'd3'
+        }
+      }
+    }
+  }
 })
